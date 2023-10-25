5b46695c8bc653a12d1200619bf27656dae91de2
yms1789 <yms1789@naver.com>
2023-10-25 09:28:37 +0900

    💄Design: 프로젝트에서 사용할 폰트 추가

--- FE/src/styles/SGlobal.ts
+++ FE/src/styles/SGlobal.ts
@@ -1,5 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
+import NanumSquareNeoLight from "..assets/fonts/NanumSquareNeo-aLt.ttf";
+import NanumSquareNeoRegular from "..assets/fonts/NanumSquareNeo-bRg.ttf";
+import NanumSquareNeoBold from "..assets/fonts/NanumSquareNeo-cBd.ttf";
+import NanumSquareNeoExtraBold from "..assets/fonts/NanumSquareNeo-dEb.ttf";
+import NanumSquareNeoHeavy from "..assets/fonts/NanumSquareNeo-eHv.ttf";
+
 export const GlobalStyle = createGlobalStyle`
   *, *::before, *::after {
     box-sizing: border-box;
@@ -26,4 +32,38 @@ export const GlobalStyle = createGlobalStyle`
       outline: none;
     }
   }
+
+  @font-face {
+        font-family: 'NanumSquareNeoLight';
+        src: local('NanumSquareNeoLight'), local('NanumSquareNeoLight');
+        font-style: normal;
+        src: url(${NanumSquareNeoLight}) format('truetype');
+  }
+  @font-face {
+        font-family: 'NanumSquareNeoRegular';
+        src: local('NanumSquareNeoRegular'), local('NanumSquareNeoRegular');
+        font-style: normal;
+        src: url(${NanumSquareNeoRegular}) format('truetype');
+  }
+  @font-face {
+        font-family: 'NanumSquareNeoBold';
+        src: local('NanumSquareNeoBold'), local('NanumSquareNeoBold');
+        font-style: normal;
+        src: url(${NanumSquareNeoBold}) format('truetype');
+  }  
+  
+  @font-face {
+        font-family: 'NanumSquareNeoExtraBold';
+        src: local('NanumSquareNeoExtraBold'), local('NanumSquareNeoExtraBold');
+        font-style: normal;
+        src: url(${NanumSquareNeoExtraBold}) format('truetype');
+  }
+
+  @font-face {
+        font-family: 'NanumSquareNeoHeavy';
+        src: local('NanumSquareNeoHeavy'), local('NanumSquareNeoHeavy');
+        font-style: normal;
+        src: url(${NanumSquareNeoHeavy}) format('truetype');
+  }
+
 `;
