1e969671cd9e99f38b5274e904fd9240761c5187
yms1789 <yms1789@naver.com>
2023-10-27 15:05:56 +0900

    🔨Refactor: 라이브러리 추가 - vite-plugin-environment 라이브러리 추가 - lodash 라이브러리 및 타입 추가 - axios 라이브러리 추가

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import EnvironmentPlugin from "vite-plugin-environment";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), EnvironmentPlugin("all")],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://openapi.naver.com",
+        rewrite: path => path.replace(/^\/api/, ""),
+        changeOrigin: true,
+      },
+    },
+  },
 });
