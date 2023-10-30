eed7feaf3912c14ab3280af0639c8a9f830412a9
yms1789 <yms1789@naver.com>
2023-10-31 08:58:18 +0900

    🚑Fix: nginx 서버 구축에 따른 코드 수정

--- FE/src/components/DrawerComponent.tsx
+++ FE/src/components/DrawerComponent.tsx
@@ -107,7 +107,7 @@ function DrawerComponent({ setCoordinates }: IDrawerComponent) {
 
   const fetchPlaces = async (query: string) => {
     try {
-      const response = await axios.get("/api/v1/search/local.json", {
+      const response = await axios.get("/naver/v1/search/local.json", {
         params: {
           query: query,
           display: 20,
