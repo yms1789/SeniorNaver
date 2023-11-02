9becbb1e638708a66470b49a480c69da66fe2db6
yms1789 <yms1789@naver.com>
2023-11-02 11:16:29 +0900

    🚀Feat: 장소 탭 기능 추가
    - 검색하거나 카테고리 클릭하면 해당 지도 위치로 이동할 수 있도록 기능 추가

--- FE/src/pages/Places.tsx
+++ FE/src/pages/Places.tsx
@@ -4,8 +4,7 @@ import NavigationBar from "../components/NavigationBar";
 import useGeolocation from "../hooks/useGeolocation";
 import { useRecoilState } from "recoil";
 import coordinateState from "../states/coordinates";
-import { styled } from "styled-components";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState, Suspense } from "react";
 export interface ICoordinate {
   mapX: string;
   mapY: string;
@@ -18,6 +17,11 @@ function Places() {
   const navermaps = useNavermaps();
   const location = useGeolocation();
   const [currentAddr, setCurrentAddr] = useState<IAddress>();
+  // const [mapState, setMapState] = useState(null);s
+  const map = useRef<naver.maps.Map>(null);
+
+  const [isWork, setIsWork] = useState(false);
+
   useEffect(() => {
     if (location.loaded) {
       console.log(location.coordinates);
@@ -36,51 +40,77 @@ function Places() {
     }
   }, [location.loaded]);
   const [coordinates, setCoordinates] = useRecoilState<ICoordinate[]>(coordinateState);
+
+  useEffect(() => {
+    if (map.current && isWork && coordinates.length) {
+      const cpCoord = [...coordinates];
+      cpCoord.sort((a: ICoordinate, b: ICoordinate) => {
+        return parseFloat(a.mapY) - parseFloat(b.mapY);
+      });
+      const [minLat, maxLat] = [cpCoord[0].mapY, cpCoord[coordinates.length - 1].mapY];
+      cpCoord.sort((a: ICoordinate, b: ICoordinate) => {
+        return parseFloat(a.mapX) - parseFloat(b.mapX);
+      });
+      const [minLng, maxLng] = [cpCoord[0].mapX, cpCoord[coordinates.length - 1].mapX];
+
+      const boundary = new navermaps.LatLngBounds(
+        new navermaps.LatLng(parseFloat(minLat), parseFloat(minLng)),
+        new navermaps.LatLng(parseFloat(maxLat), parseFloat(maxLng)),
+      );
+      map.current.fitBounds(boundary);
+    }
+    setIsWork(false);
+  }, [isWork]);
+
   return (
     <>
-      <MapDiv
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          overflow: "hidden",
-        }}
-      >
-        {location.loaded && (
-          <NaverMap
-            defaultCenter={
-              new navermaps.LatLng(location.coordinates!.lat, location.coordinates!.lng)
-            }
-            defaultZoom={18}
-            disableKineticPan={false}
-            zoomControl
-            zoomControlOptions={{
-              position: navermaps.Position.TOP_RIGHT,
-            }}
-            minZoom={8}
-            maxZoom={21}
-          >
-            {coordinates.length &&
-              coordinates.map((coordinate, idx) => {
-                const lat = coordinate.mapY;
-                const lng = coordinate.mapX;
-                return (
-                  <Marker
-                    key={`좌표${idx}`}
-                    position={new navermaps.LatLng(parseFloat(lat), parseFloat(lng))}
-                  />
-                );
-              })}
-            <Marker
-              position={new navermaps.LatLng(location.coordinates!.lat, location.coordinates!.lng)}
-            />
-          </NaverMap>
-        )}
-      </MapDiv>
-      <DrawerComponent setCoordinates={setCoordinates} currentAddr={currentAddr} />
-      <NavigationBar />
+      <Suspense fallback={<div>로딩 중...</div>}>
+        <MapDiv
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            overflow: "hidden",
+          }}
+        >
+          {location.loaded && (
+            <NaverMap
+              ref={map}
+              defaultCenter={
+                new navermaps.LatLng(location.coordinates!.lat, location.coordinates!.lng)
+              }
+              defaultZoom={10}
+              disableKineticPan={false}
+              zoomControl
+              zoomControlOptions={{
+                position: navermaps.Position.TOP_RIGHT,
+              }}
+              minZoom={8}
+              maxZoom={13}
+            >
+              {coordinates.length &&
+                coordinates.map((coordinate, idx) => {
+                  const lat = coordinate.mapY;
+                  const lng = coordinate.mapX;
+                  return (
+                    <Marker
+                      key={`좌표${idx}`}
+                      position={new navermaps.LatLng(parseFloat(lat), parseFloat(lng))}
+                    />
+                  );
+                })}
+            </NaverMap>
+          )}
+        </MapDiv>
+        <DrawerComponent
+          setCoordinates={setCoordinates}
+          currentAddr={currentAddr}
+          setIsWork={setIsWork}
+        />
+        <NavigationBar />
+      </Suspense>
     </>
   );
 }
