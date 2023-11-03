9a1cff59570ed12865c094b5c6ec77c24b87e17c
yms1789 <yms1789@naver.com>
2023-11-03 15:42:31 +0900

    🚀Feat: 로딩 페이지 구현

---
+++ FE/src/pages/Loading.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import Spinner from "../assets/images/spinner.gif";
+
+export const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 50vh;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LoadingText = styled.div`
+  font: 1rem NanumSquareNeoBold;
+  text-align: center;
+`;
+
+function Loading() {
+  return (
+    <Background>
+      <LoadingText>잠시만 기다려 주세요.</LoadingText>
+      <img src={Spinner} alt="로딩중" width="5%" />
+    </Background>
+  );
+}
+
+export default Loading;
