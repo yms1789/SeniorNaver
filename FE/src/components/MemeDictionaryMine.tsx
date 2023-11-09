3af8a56ae1efe0b102577b1fddf4beb6cb626447
hotchapa <hotchapa@naver.com>
2023-11-09 15:13:26 +0900

    🚀Feat: 유행어사전 내 단어장 탭 추가

--- FE/src/components/MemeDictionaryMine.tsx
+++ FE/src/components/MemeDictionaryMine.tsx
@@ -1,16 +1,50 @@
 import styled from "styled-components";
+import { useRecoilState,useSetRecoilState } from "recoil";
+import MemeDictionaryMineSelector from "./MemeDictionaryMineSelector";
+import { memeMineCurrentCategoryState } from "../states/useMeme";
+import MemeDictionaryMineSavedWords from "./MemeDictionaryMineSavedWords";
+import MemeDictionaryMineProblems from "./MemeDictionaryMineProblems";
+import MemeDictionaryMineReport from "./MemeDictionaryMineReport";
+
 const MemeDictionaryMineWraaper = styled.div`
+  margin-top: 100px;
   width: 930px;
-  height: 100vh;
+  height: 100%;
   display: flex;
+  align-items: flex-start;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
 `
 
 function MemeDictionaryMine() {
+  const currentCategory = useRecoilState(memeMineCurrentCategoryState);
+  switch (currentCategory[0].currentCategory) {
+    case 0:
   return (
-    <MemeDictionaryMineWraaper>MemeDictionaryMine</MemeDictionaryMineWraaper>
+    <MemeDictionaryMineWraaper> 
+      <MemeDictionaryMineSelector/>
+    </MemeDictionaryMineWraaper>
   )
+  case 1:
+    return (
+      <MemeDictionaryMineWraaper> 
+        <MemeDictionaryMineSavedWords/>
+      </MemeDictionaryMineWraaper>
+    )
+  case 2:
+    return (
+      <MemeDictionaryMineWraaper> 
+        <MemeDictionaryMineProblems/>
+      </MemeDictionaryMineWraaper>
+    )
+  case 3:
+    return (
+      <MemeDictionaryMineWraaper> 
+        <MemeDictionaryMineReport/>
+      </MemeDictionaryMineWraaper>
+    )
+  default:
+  break;
+}
 }
 export default MemeDictionaryMine;
