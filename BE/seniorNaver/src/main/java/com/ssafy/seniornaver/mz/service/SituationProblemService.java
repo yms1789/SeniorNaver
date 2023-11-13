a2b6eca8f6790be301b777d382f01fcebfd9e23f
KwonMinWoo <kwmw0427@naver.com>
2023-11-13 17:56:24 +0900

    🚑Fix: 문제 리스트 기능 추가

--- BE/seniorNaver/src/main/java/com/ssafy/seniornaver/mz/service/SituationProblemService.java
+++ BE/seniorNaver/src/main/java/com/ssafy/seniornaver/mz/service/SituationProblemService.java
@@ -2,10 +2,14 @@ package com.ssafy.seniornaver.mz.service;
 
 import com.ssafy.seniornaver.auth.entity.Member;
 import com.ssafy.seniornaver.mz.dto.request.ProblemCreateRequestDto;
+import com.ssafy.seniornaver.mz.dto.response.ProblemListResponseDto;
 import com.ssafy.seniornaver.mz.entity.SituationProblem;
+
+import java.util.List;
 
 public interface SituationProblemService {
     boolean wordCheck(String word);
     SituationProblem createProblem(ProblemCreateRequestDto problemCreateRequestDto, Member member);
     void relTagToProblem(SituationProblem situationProblem, ProblemCreateRequestDto problemCreateRequestDto);
+    List<ProblemListResponseDto> getProblemList(ProblemCreateRequestDto problemCreateRequestDto);
 }
