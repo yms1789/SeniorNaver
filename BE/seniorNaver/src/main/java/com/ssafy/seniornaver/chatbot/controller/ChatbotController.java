c63f3b6e11fbc3ef941894149d3ac7f1415f68cd
김민석 <koo3435@gmail.com>
2023-11-13 12:40:37 +0900

    🚑Fix: 챗봇 데이터 변경

--- BE/seniorNaver/src/main/java/com/ssafy/seniornaver/chatbot/controller/ChatbotController.java
+++ BE/seniorNaver/src/main/java/com/ssafy/seniornaver/chatbot/controller/ChatbotController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Arrays;
-import java.util.Base64;
 
 @Slf4j
 @RestController
@@ -26,7 +25,7 @@ public class ChatbotController {
 
     @PostMapping(value="/v1/talk",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     @Operation(summary = "음성 텍스트 변형", security = @SecurityRequirement(name = "Bearer"))
-    public ResponseEntity<String> convertAndTalk(@RequestPart("voiceFile") MultipartFile voiceFile) {
+    public ResponseEntity<byte[]> convertAndTalk(@RequestPart("voiceFile") MultipartFile voiceFile) {
 
         // 파일 이름 확인
         String originalFilename = voiceFile.getOriginalFilename();
@@ -40,17 +39,11 @@ public class ChatbotController {
         log.info("테스트3 " + response);
         // 챗봇의 응답을 음성으로 변환
         byte[] voiceData = chatbotService.convertTextToSpeech(response);
-
-//        log.info(Arrays.toString(voiceData));
-        // 음성 데이터를 Base64로 인코딩
-        String encodedVoiceData = Base64.getEncoder().encodeToString(voiceData);
-
-//        log.info("Encoded voice data: " + encodedVoiceData);
-
+        
         // 음성 데이터를 HTTP 응답 본문에 담아 반환
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_TYPE, "application/json")
-                .body("{\"voiceData\":\"" + encodedVoiceData + "\"}");
+                .header(HttpHeaders.CONTENT_TYPE, "audio/mpeg")
+                .body(voiceData);
 //        return response;
     }
 
