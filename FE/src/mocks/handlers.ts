19a8a7470bfa6f9d8b713f5d1b28b313b0db5f3a
kanghaeven <meerhae@gmail.com>
2023-11-14 16:15:23 +0900

    🚑Fix: 실제 api 연결한 msw 코드 삭제

--- FE/src/mocks/handlers.ts
+++ FE/src/mocks/handlers.ts
@@ -1,6 +1,4 @@
 import { rest } from "msw";
-import hoveredImg from "../assets/Frame 185.png";
-import nonhoveredImg from "../assets/Frame 186.png";
 
 export const handlers = [
   rest.get("/test/places", (req, res, ctx) => {
@@ -244,66 +242,5 @@ export const handlers = [
     if (req.url.searchParams.get("workplace") && req.url.searchParams.get("keyword"))
       return res(ctx.delay(1000), ctx.json(response), ctx.status(200));
     return res(ctx.delay(1000), ctx.json(response2), ctx.status(200));
-  }),
-
-  rest.get("/carousel", (req, res, ctx) => {
-    return res(
-      ctx.json({
-        curationImages: [
-          "https://newsimg-hams.hankookilbo.com/2023/10/30/20e18974-774c-4f89-b1f4-e3861bdab60f.jpg",
-          "https://imgnews.pstatic.net/image/629/2023/11/10/202339301699570052_20231110074901855.jpg?type=w647",
-          "https://res.heraldm.com/content/image/2022/05/09/20220509000805_0.jpg",
-          "https://flexible.img.hani.co.kr/flexible/normal/718/431/imgdb/child/2022/0728/53_16589695226816_5916589695085075.jpg",
-          "https://src.hidoc.co.kr/image/lib/2016/1/13/20160113103847160_0.jpg",
-          "https://www.junggi.co.kr/data/article/20231110/654d9c6e9114d.jpg",
-          "https://imgnews.pstatic.net/image/032/2023/11/10/0003260416_001_20231110120901096.jpeg?type=w647",
-          "https://imgnews.pstatic.net/image/088/2023/11/10/0000845239_001_20231110123601189.png?type=w647",
-          "https://cdn.lkp.news/news/photo/202311/38859_50849_1221.png",
-          "https://cdnimage.ebn.co.kr/news/AKR1202311101200239910/news-p.v1.20231110.ab181baadd8b4bca9d4042726933ad42_P1.png",
-        ],
-        curationTexts: [
-          "밤 되면 이 가수 공연장에 등장하는 '효도텐트'",
-          "영웅시대 밴드(나눔모임), 56번째 쪽방촌 도시락 봉사",
-          "“5060 잡았다” 불황 모르는 팬덤 경제",
-          "더 건강하고 부유해진 젊은 노인들, 소비패권 쥐게 된다",
-          "성형은 젊은 층의 전유물이 아니다.'회춘성형'",
-          "밤이 더 아름다운 여행…‘대한민국 밤밤곡곡’",
-          "주말에 붉게 물든 단풍 보러 갈까 했더니 ‘영하권’",
-          "“격무, 스트레스에 실명”, 악성민원 시달린 공무원 국가유공자로 인정",
-          "대법원장·헌재소장 공백 현실화",
-          "HCN “지역 밀착·참여형 오리지널 콘텐츠 인기”",
-        ],
-        mzWords: [
-          "탕후루",
-          "알잘딱깔센",
-          "어쩔티비",
-          "파이어족",
-          "치맥",
-          "저메추",
-          "얼죽아",
-          "아샷추",
-          "점메추",
-          "아아",
-        ],
-        places: [
-          [
-            "https://a.cdn-hotels.com/gdcs/production60/d893/3172bd6f-726c-4561-810f-deec13d17a6e.jpg?impolicy=fcrop&w=800&h=533&q=medium",
-            "경복궁",
-          ],
-          ["http://tong.visitkorea.or.kr/cms/resource/85/2031885_image2_1.jpg", "감로암"],
-          ["http://tong.visitkorea.or.kr/cms/resource/36/1984636_image2_1.jpg", "길산근린공원"],
-          ["http://tong.visitkorea.or.kr/cms/resource/62/2612062_image2_1.bmp", "거리공원"],
-          ["http://tong.visitkorea.or.kr/cms/resource/84/2791384_image2_1.jpg", "거북선나루터"],
-          ["http://tong.visitkorea.or.kr/cms/resource/36/1984636_image2_1.jpg", "건청궁"],
-          ["http://tong.visitkorea.or.kr/cms/resource/81/1894481_image2_1.jpg", "강서습지생태공원"],
-          ["http://tong.visitkorea.or.kr/cms/resource/36/1984636_image2_1.jpg", "경운동민병옥가옥"],
-          [
-            "http://tong.visitkorea.or.kr/cms/resource/59/1568059_image2_1.jpg",
-            "고당조만식선생동상",
-          ],
-          ["http://tong.visitkorea.or.kr/cms/resource/36/1984636_image2_1.jpg", "경희궁공원"],
-        ],
-      }),
-    );
   }),
 ];
