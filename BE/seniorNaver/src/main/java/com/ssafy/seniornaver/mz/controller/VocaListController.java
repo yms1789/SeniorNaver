cd97bd8790e1f01ae8edac068357fea39a4f67b5
KwonMinWoo <kwmw0427@naver.com>
2023-11-15 17:28:41 +0900

    🚑Fix: 리스트 fix 9

--- BE/seniorNaver/src/main/java/com/ssafy/seniornaver/mz/controller/VocaListController.java
+++ BE/seniorNaver/src/main/java/com/ssafy/seniornaver/mz/controller/VocaListController.java
@@ -40,7 +40,7 @@ public class VocaListController {
     @Operation(summary = "단어장 목록 불러오기", description = "category | 1 = 스크랩 단어,  2 = 저장 문제 , 3 = 만든 문제 " +
             " | 페이지는 0부터 시작")
     @GetMapping("/list")
-    public ResponseEntity<VocaListResponseDto> getScrapWordList(VocaListRequestDto vocaListRequestDto,
+    public ResponseEntity<VocaListResponseDto> getScrapWordList(@RequestBody VocaListRequestDto vocaListRequestDto,
                                                                 HttpServletRequest httpServletRequest) {
 
         VocaListResponseDto scrapWordResponseDto = vocabularyListService.getVocaList(getMember(httpServletRequest), vocaListRequestDto);
