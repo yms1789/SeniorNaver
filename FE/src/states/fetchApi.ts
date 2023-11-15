82fb687b28531ccfe0f48e9e6b7529dbbffc3ab1
hotchapa <hotchapa@naver.com>
2023-11-15 16:32:13 +0900

    🚑Fix: fetchApi 수정 - 비로그인 상태에서도 일반 axios 요청이 가능하도록 코드를 수정

--- FE/src/states/fetchApi.ts
+++ FE/src/states/fetchApi.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosError, AxiosRequestHeaders } from "axios";
-import { useRecoilValue } from "recoil";
 import { fetchToken, useLogout, userState } from "./useUser";
 
 interface AdaptAxiosRequestConfig extends AxiosRequestConfig {
@@ -17,7 +16,7 @@ fetchApi.interceptors.request.use(
   async (config): Promise<AdaptAxiosRequestConfig> => {
     const persistData = JSON.parse(sessionStorage.getItem("recoil-persist") || "{}"); // 세션 저장소에서 토큰 액세스
     const user = persistData.userInfo;
-    if (user.accessToken) {
+    if (user && user.accessToken) {
       config.headers = config.headers || {};
       config.headers.Authorization = `Bearer ${user.accessToken}`; // 현재 토큰을 헤더에 설정
     }
@@ -74,37 +73,3 @@ fetchApi.interceptors.response.use(
 );
 
 export default fetchApi;
-// import axios, { AxiosInstance } from "axios";
-// import { fetchToken, useLogout, userState } from "./useUser";
-
-// const fetchApi = (): AxiosInstance => {
-//   const user = JSON.parse(sessionStorage.getItem("userInfo") || "{}"); // 로컬 저장소에서 토큰 액세스
-//   const refreshTokenData = {
-//     accessToken: user.accessToken,
-//     refreshToken: user.refreshToken,
-//   };
-//   const logout = useLogout(refreshTokenData);
-//   const axiosInstance = axios.create({
-//     // baseURL: "http://127.0.0.1:8000/",
-//     headers: {
-//       Authorization: `Bearer ${refreshTokenData.accessToken}`,
-//     },
-//   });
-
-//   axiosInstance.interceptors.response.use(
-//     response => response,
-//     error => {
-//       const statusCode = error.response?.status;
-//       if (statusCode === 401) {
-//         error.response.statusText = "Unauthorized";
-//         error.response.status = 401;
-//         logout();
-//       }
-//       return Promise.reject(error);
-//     },
-//   );
-
-//   return axiosInstance;
-// };
-
-// export default fetchApi;
