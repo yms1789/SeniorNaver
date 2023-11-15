987a16d7c1cbfa85339304f4889302c1ee9ae467
김민석 <koo3435@gmail.com>
2023-11-15 17:26:13 +0900

    🚑Fix: 엑세스 토큰 시간 변경

--- BE/seniorNaver/src/main/java/com/ssafy/seniornaver/auth/jwt/JwtProvider.java
+++ BE/seniorNaver/src/main/java/com/ssafy/seniornaver/auth/jwt/JwtProvider.java
@@ -18,7 +18,9 @@ import java.util.HashMap;
 public class JwtProvider {
     private final MemberRepository memberRepository;
     @Value("${jwt.secret.key}") private String secret;
-    private static final Long ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60L; // 1 hours 1000 * 60 * 60L
+//    private static final Long ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60L; // 1 hours 1000 * 60 * 60L
+    private static final Long ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 30L; // 30 seconds
+
     private static final Long REFRESH_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 30L; // 30 days
 
     public TokenDto createAccessToken(String memberId, AuthProvider provider) {
